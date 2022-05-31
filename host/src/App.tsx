9d75bf1c3b9ba785b1b555784fa313556a040e20
강성훈 <80798929+ksh5324@users.noreply.github.com>
2022-05-31 10:10:11 +0900

    router

--- host/src/App.tsx
+++ host/src/App.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { useSelector } from "react-redux";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 
@@ -9,12 +10,17 @@ import { StoreProvider } from "store/store";
 const App = () => {
   const { login } = useSelector((state: any) => state.user);
   return (
-    <div className="container">
-      <div>Name: host {login ? "sdf" : "sf"}</div>
-      <div>Framework: react</div>
-      <div>Language: TypeScript</div>
-      <div>CSS: Empty CSS</div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<div>s</div>} />
+        <Route path="/profile" element={<div>a</div>} />
+        <Route path="/mypost" element={<div>b</div>} />
+        <Route path="/post/*" element={<div>sdf</div>}>
+          <Route path=":id" element={<div>sef</div>} />
+        </Route>
+        <Route path="/*" element={<div>not</div>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 ReactDOM.render(
