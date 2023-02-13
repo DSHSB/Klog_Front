9a08e9b788a0135fb2b38a10338b9f8da4c85e5e
Park Ji Gun <79364171+bjigeon@users.noreply.github.com>
2023-02-13 19:21:38 +0900

    create writerInfo in postid

--- src/components/common/Post/Post.tsx
+++ src/components/common/Post/Post.tsx
@@ -3,16 +3,27 @@ import ReactMarkdown from "react-markdown";
 import * as P from "./PostStyle";
 import LOVE from "../../../assets/image/WriteList/Vector.png";
 import Kang from "../../../assets/image/Profile/Kang.jpeg";
-import { useLocation } from "react-router";
+import GitHub from "../../../assets/image/Post/GitHub.png";
+import Mail from "../../../assets/image/Post/Mail.png";
+
+import { useLocation, useNavigate } from "react-router";
 
 const Post = () => {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const [title, setTitle] = useState("강성훈이 천재인 이유");
   const [categorie, setCategorie] = useState(["backend", "forntend"]);
   const [love, setLove] = useState("137");
   const [contnet, setContnet] = useState(
     "dfas&nbsp;&nbsp;&nbsp;dfsafdasdfsafsafsadf\n ## fsadf\n### fsadfsadf\n#### fsadfasd\n# adfsadfasdfsdfadfsad"
   );
+  const [writerInfo, setWriterInfo] = useState({
+    img: "../../../assets/image/Profile/Kang.jpeg",
+    name: "강성훈",
+    introduce: "고등학생 천재 주니어 개발자",
+    connect: { git: "https://github.com/ksh5324", mail: "ksh5324@gmail.com" },
+  });
 
   useEffect(() => {}, [location]);
 
@@ -22,7 +33,7 @@ const Post = () => {
         <h1>{title}</h1>
         <P.CategoriesContainer>
           {categorie.map((i, idx) => {
-            return <P.CategoriesBox>{i}</P.CategoriesBox>;
+            return <P.CategoriesBox key={idx}>{i}</P.CategoriesBox>;
           })}
         </P.CategoriesContainer>
         <P.LoveContainer>
@@ -30,14 +41,36 @@ const Post = () => {
           {love}
         </P.LoveContainer>
         <P.MarkdownContainer>
-          <img src={Kang} />
           <div className="markdown--container">
             <div className="markdown">
               <ReactMarkdown>{contnet}</ReactMarkdown>
             </div>
           </div>
         </P.MarkdownContainer>
-        <P.WriterInfo></P.WriterInfo>
+        <P.WriterInfo>
+          <div>
+            <img src={Kang} />
+            <div>
+              <h3>{writerInfo.name}</h3>
+              <p>{writerInfo.introduce}</p>
+            </div>
+          </div>
+        </P.WriterInfo>
+        <hr />
+        <P.ConnectIconContaier>
+          {writerInfo.connect.hasOwnProperty("mail") ? (
+            <img title={writerInfo.connect.mail} src={Mail} />
+          ) : null}
+          {writerInfo.connect.hasOwnProperty("git") ? (
+            <img
+              id="gitIcon"
+              src={GitHub}
+              onClick={() => {
+                window.open(`${writerInfo.connect.git}`);
+              }}
+            />
+          ) : null}
+        </P.ConnectIconContaier>
       </P.Post>
     </P.PostContainer>
   );
--- src/components/common/Post/PostStyle.ts
+++ src/components/common/Post/PostStyle.ts
@@ -75,20 +75,54 @@ export const LoveContainer = styled.div`
 
 export const MarkdownContainer = styled.div`
   margin-top: 70px;
-
-  img {
-    height: 590px;
-  }
 `;
 
 export const WriterInfo = styled.div`
   width: auto;
+  /* height: 315px; */
+  margin-top: 100px;
+
+  display: flex;
+  flex-direction: column;
+
+  div {
+    display: flex;
+    /* align-items: center; */
+
+    img {
+      height: 165px;
+      width: 165px;
+      border-radius: 100%;
+    }
+
+    div {
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+
+      margin-left: 20px;
+      h3 {
+        margin: 0;
+      }
+
+      p {
+      }
+    }
+  }
+
+  hr {
+  }
+`;
+
+export const ConnectIconContaier = styled.div`
   display: flex;
 
   img {
-    h3 {
-    }
-    p {
-    }
+    width: 50px;
+    height: 50px;
+  }
+
+  img[id="gitIcon"] {
+    cursor: pointer;
   }
 `;
